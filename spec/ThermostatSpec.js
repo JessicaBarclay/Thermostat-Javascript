d36ababa5c0a0b4c4031011124411f3bdf1075f8
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-09 17:04:59 +0100

    Thermostat function created. Has temperature property

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,12 @@
+describe('Thermostat', function () {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('should start with a temperature of 20 degrees', function(){
+    expect(thermostat.temperature).toBe(20);
+  });
+
+});
