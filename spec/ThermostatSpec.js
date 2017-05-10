902919ffb35898756dfe15bb89b654d06f23c93e
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-10 16:38:23 +0100

    Renames tests

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,32 +5,29 @@ describe('Thermostat', function () {
     thermostat = new Thermostat();
   });
 
-  it('should start with a temperature of 20 degrees', function(){
+  it('starts with temperature set to 20 degrees', function(){
     expect(thermostat.temperature).toBe(20);
   });
 
-  it('should increases the temperature from 20 to 21 degrees', function(){
+  it('increases temperature by 1', function(){
     thermostat.increaseTemp();
     expect(thermostat.temperature).toBe(21);
   });
 
-  it('should descrease the temperature from 20 to 19 degrees', function(){
+  it('decreases temperature by 1', function(){
     thermostat.decreaseTemp();
     expect(thermostat.temperature).toBe(19);
   });
 
-  it('should have a minimum temperature value of 10 degrees', function(){
+  it('has a minimum temperature value of 10 degrees', function(){
     expect(thermostat.minTemp).toBe(10);
   });
 
-  it('should not be able to decrease the temperature below 10 degrees', function(){
-
+  it('cannot decrease the temperature below 10 degrees', function(){
     for (var i=0; i<=11; i++) {
       thermostat.decreaseTemp();
       console.log(thermostat.temperature);
     }
-
     expect(thermostat.temperature).toBe(10);
   });
-
 });
